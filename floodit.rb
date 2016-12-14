4c8aca73f36bc8b5828cc96c04a7a91b6d9c3e33
Charlie Lee <charlielee735@gmail.com>
2016-12-14 12:02:34 +0000

    Create Gemfile with bundler

---
+++ floodit.rb
@@ -0,0 +1,22 @@
+
+def get_board(width, height)
+  # TODO: Implement this method
+  #
+  # This method should return a two-dimensional array.
+  # Each element of the array should be one of the
+  # following values (These are "symbols", you can use
+  # them like constant values):
+  # :red
+  # :blue
+  # :green
+  # :yellow
+  # :cyan
+  # :magenta
+  #
+  # It is important that this method is used because
+  # this will be used for checking the functionality
+  # of your implementation.
+end
+
+# TODO: Implement everything else as described in the
+#       assignment brief.
